42bb412d6e6ce278071dcbf0c79642a59d860a76
[carlos martinez] <theycallhellp@gmail.com>
2022-12-07 18:26:50 -0800

    client side login graphql refactor

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
 // const routes = require('./routes');
 
 const app = express();
